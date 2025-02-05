c368fc0132e809d2ecf8b760c4add0513e684308
Ville Ylikangas <villeylik@gmail.com>
2025-02-05 11:16:10 +0200

    add localstorage, basic html and show todos

--- src/index.js
+++ src/index.js
@@ -13,8 +13,27 @@ class Todo {
 const addTodo = (title, desc, dueDate, priority, project = 'general') => {
   const newTodo = new Todo(title, desc, dueDate, priority, project);
   todos.push(newTodo);
+  localStorage.setItem('odinTodos', JSON.stringify(todos));
 };
 
-let longDate = new Date(Date.now());
-let shortDate = longDate.toDateString();
-addTodo('clean code', 'refactor everything', shortDate, 'low');
+const showTodos = () => {
+  const todoCont = document.getElementById('todos-container');
+  let todos = localStorage.getItem('odinTodos');
+  if (todos) {
+    todos = JSON.parse(todos);
+    todos.forEach((todo) => {
+      const todoDiv = document.createElement('div');
+      todoDiv.classList.add('todo-item');
+      todoDiv.innerHTML = `
+        <h3>${todo.title}</h3>
+        <p>Description: ${todo.description}</p>
+        <p>Due: ${todo.dueDate}</p>
+        <p>Priority: ${todo.priority}</p>
+        <p>Project: ${todo.project}</p>
+      `;
+      todoCont.appendChild(todoDiv);
+    });
+  }
+};
+
+showTodos();

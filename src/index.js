643d2602235aadcbc7826cae8f1076bdd136b3ff
Ville Ylikangas <villeylik@gmail.com>
2025-02-05 15:42:26 +0200

    refactor code, todo and displaycontroller modules

--- src/index.js
+++ src/index.js
@@ -1,28 +1,42 @@
-const todos = [];
+const TodoModule = (function () {
+  const todos = JSON.parse(localStorage.getItem('odinTodos')) || [];
 
-class Todo {
-  constructor(title, description, dueDate, priority, project) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.project = project;
+  class Todo {
+    constructor(title, description, dueDate, priority, project) {
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate;
+      this.priority = priority;
+      this.project = project || general;
+    }
   }
-}
 
-const addTodo = (title, desc, dueDate, priority, project = 'general') => {
-  const newTodo = new Todo(title, desc, dueDate, priority, project);
-  todos.push(newTodo);
-  localStorage.setItem('odinTodos', JSON.stringify(todos));
-};
+  const addTodo = (title, desc, dueDate, priority, project = 'general') => {
+    const newTodo = new Todo(title, desc, dueDate, priority, project);
+    todos.push(newTodo);
+    saveTodos();
+  };
 
-const showTodos = () => {
-  const todoCont = document.getElementById('todos-container');
-  let todos = localStorage.getItem('odinTodos');
-  if (todos) {
-    todos = JSON.parse(todos);
-    todos.forEach((todo) => {
-      const todoDiv = document.createElement('div');
+  const removeTodo = (index) => {
+    todos.splice(index, 1);
+    saveTodos();
+  };
+
+  const getTodos = () => todos;
+
+  const saveTodos = () => {
+    localStorage.setItem('odinTodos', JSON.stringify(todos));
+  };
+
+  return { addTodo, removeTodo, getTodos };
+})();
+
+const DisplayController = (function () {
+  const renderTodos = () => {
+    const todoContainer = document.getElementById('todos-container');
+    todoContainer.innerHTML = '';
+
+    TodoModule.getTodos().forEach((todo, index) => {
       todoDiv.classList.add('todo-item');
       todoDiv.innerHTML = `
         <h3>${todo.title}</h3>
@@ -30,10 +44,19 @@ const showTodos = () => {
         <p>Due: ${todo.dueDate}</p>
         <p>Priority: ${todo.priority}</p>
         <p>Project: ${todo.project}</p>
+        <button class="delete-btn" data-index="${index}">Delete</button>
       `;
-      todoCont.appendChild(todoDiv);
+      todoContainer.appendChild(todoDiv);
     });
-  }
-};
 
-showTodos();
+    document.querySelectorAll('.delete-btn').forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+        const index = e.target.dataset.index;
+        TodoModule.removeTodo(index);
+        renderTodos();
+      });
+    });
+  };
+
+  return { renderTodos };
+})();
